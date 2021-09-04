76790ab17b50ecfd8b9a39c2839cca38eaace519
Pedro Franco de Carvalho <pedro@pedromfc.com.br>
2021-09-04 13:38:59 -0300

    db: Add commands to create accounts table

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,22 @@
 CREATE DATABASE pedro_bank;
+
+\c pedro_bank
+
+CREATE TABLE accounts (
+    -- Start at 1 to avoid errors when unmarshalling json with no id field,
+    -- which would become 0.
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START 1),
+    name VARCHAR(32) NOT NULL,
+    -- XXX.XXX-XX: a "fake" CPF with 8 digits (to avoid accidentaly using a
+    -- real person's CPF)
+    cpf CHAR(10) NOT NULL UNIQUE,
+    -- 32 bytes (256 bits) for a sha256 hash of the password, represented as
+    -- 64 hex digits 
+    secret CHAR(64) NOT NULL,
+    -- We represent the balance as integers where the last two digits are the
+    -- BRL cents. We don't need more precision sicne we only add/substract
+    -- from the account balance.
+    balance INTEGER NOT NULL,
+    -- No time zone, store always as UTC
+    created_at TIMESTAMP NOT NULL
+);
