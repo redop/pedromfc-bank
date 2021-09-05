3703d6c456a0e17f2f0f90825afb33c268c29f3a
Pedro Franco de Carvalho <pedro@pedromfc.com.br>
2021-09-05 14:22:16 -0300

    server: Run server in a goroutine
    Also register sigint to stop gracefully the server.

--- src/main/main.go
+++ src/main/main.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
+	"os/signal"
 	"pedro-bank/server"
 )
 
 func main() {
+	sigintStop := make(chan os.Signal, 1)
+	signal.Notify(sigintStop, os.Interrupt)
+
 	var certsDir string
 	flag.StringVar(&certsDir, "certs", ".",
 		"Directory with key.pem and cert.pem")
 
 	flag.Parse()
 
-	server.Run(certsDir)
+	go server.Run(certsDir)
+
+	<-sigintStop
+	close(sigintStop)
+	fmt.Println("Got sigint")
+
+	server.Stop()
+
+	<-server.ServerFinished
 }
